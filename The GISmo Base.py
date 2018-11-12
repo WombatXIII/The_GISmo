87f08693b937f177e870d4cf4d955d54558a4dc9
Mike J <mjohnsonthirteen@gmail.com>
2018-11-12 22:20:58 +0000

    A proper tab system with quit button

--- The GISmo Base.py
+++ The GISmo Base.py
@@ -1,22 +1,31 @@
+
+import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk #apparently it needs both
 
-# Hello
+blarg = tk.Tk()
+blarg.title("Father Jack Simulator")
 
-root = Tk()
-root.title("The GISmo")
+argyle = Frame(blarg)
+argyle.grid(column=0, row=0, sticky=(N, W, E, S))
+argyle.columnconfigure(0, weight=1)
+argyle.rowconfigure(0, weight=1)
+argyle.pack(pady=100, padx=100)
 
 
-mainframe = Frame(root)
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-mainframe.pack(pady=100, padx=100)
-
-notes = ttk.Notebook(mainframe)
+notes = ttk.Notebook(blarg)
 f1 = ttk.Frame(notes)   # first page, which would get widgets gridded into it
 f2 = ttk.Frame(notes)   # second page
-notes.add(f1, text='Bum')
-notes.add(f2, text='arse')
+f3 = ttk.Frame(notes)
+f4 = ttk.Frame(notes)
+notes.add(f1, text='Gobshite!')
+notes.add(f2, text='Arse')
+notes.add(f3, text='Feck')
+notes.add(f4, text='I love my brick')
+notes.pack(expand=1, fill="both")
+
+
+ttk.Button(argyle, text="Quit", command=blarg.destroy).grid (column=1, row=1,sticky=E)
+
 
 print ('hello..its all done')
