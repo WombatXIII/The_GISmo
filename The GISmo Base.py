1507426d5c0dd4e1fec1c2600cdee01e015b3bb2
Matt English <matthew.english@softwire.com>
2018-11-14 21:55:56 +0000

    hacked in scaleTabs to tabs

--- The GISmo Base.py
+++ The GISmo Base.py
@@ -2,8 +2,9 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk #apparently it needs both
+from ScaleFrame import *
 
-blarg = tk.Tk()
+blarg = Tk()
 blarg.title("Father Jack Simulator")
 
 argyle = Frame(blarg)
@@ -18,14 +19,17 @@ f1 = ttk.Frame(notes)   # first page, which would get widgets gridded into it
 f2 = ttk.Frame(notes)   # second page
 f3 = ttk.Frame(notes)
 f4 = ttk.Frame(notes)
+
+ScaleFrameClass = GisScaleFrame()
+scaleFrame = ScaleFrameClass.GetScaleFrame(notes)
+
 notes.add(f1, text='Gobshite!')
 notes.add(f2, text='Arse')
 notes.add(f3, text='Feck')
 notes.add(f4, text='I love my brick')
+notes.add(scaleFrame, text='Scale')
 notes.pack(expand=1, fill="both")
-
 
 ttk.Button(argyle, text="Quit", command=blarg.destroy).grid (column=1, row=1,sticky=E)
 
-
-print ('hello..its all done')
+blarg.mainloop()
