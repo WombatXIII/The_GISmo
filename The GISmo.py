d9f1b191e56e5659d4826e2f6cc15e3ec762f943
Mike J <mjohnsonthirteen@gmail.com>
2018-11-15 17:38:17 +0000

    Added Inch Converter (bit buggy)
    a 2nd textbox now opens, not sure why. now just 'the GISmo'

---
+++ The GISmo.py
@@ -0,0 +1,32 @@
+
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk #apparently it needs both
+from ScaleFrame import *
+from inches_to_mm import *
+
+blarg = Tk()
+blarg.title("The GISmo")
+
+argyle = Frame(blarg)
+argyle.grid(column=0, row=0, sticky=(N, W, E, S))
+argyle.columnconfigure(0, weight=1)
+argyle.rowconfigure(0, weight=1)
+argyle.pack(pady=100, padx=100)
+
+
+notes = ttk.Notebook(blarg)
+
+ScaleFrameClass = GisScaleFrame()
+scaleFrame = ScaleFrameClass.GetScaleFrame(notes)
+
+InchTommclass = InchTomm()
+InchTomm = InchTommclass.GetInchTomm(notes)
+
+notes.add(InchTomm, text='Inches to mm')
+notes.add(scaleFrame, text='Scale')
+notes.pack(expand=1, fill="both")
+
+ttk.Button(argyle, text="Quit", command=blarg.destroy).grid (column=1, row=1,sticky=E)
+
+blarg.mainloop()
---
+++ inches_to_mm.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class InchTomm:
+
+    def GetInchTomm(self, master):
+
+        def calculate(*args):
+            try:
+                value = float(inches.get())
+
+                mm.set(25.4 * value)
+            except ValueError:
+                pass
+        root = Tk()
+
+        mainframe = Frame(master)
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe.columnconfigure(0, weight=1)
+        mainframe.rowconfigure(0, weight=1)
+
+        inches = StringVar()
+        mm = StringVar()
+
+        inches_entry = ttk.Entry(mainframe, width=7, textvariable=inches)
+        inches_entry.grid(column=2, row=1, sticky=(W, E))
+
+        ttk.Label(mainframe, textvariable=mm).grid(column=2, row=2, sticky=(W, E))
+        ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+        ttk.Label(mainframe, text="inches").grid(column=3, row=1, sticky=W)
+        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+        ttk.Label(mainframe, text="mm").grid(column=3, row=2, sticky=W)
+
+        for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+        root.bind('<Return>', calculate)
+
+        return mainframe
+
+def say_hi(self):
+    print ("Why, hello there!")
