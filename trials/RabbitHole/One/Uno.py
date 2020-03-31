d93eeb291143bdef27e2cab00308f4dc0bbe0040
Unknown <mjohnsonthirteen@gmail.com>
2020-03-31 14:29:16 +0100

    other stuff

--- trials/RabbitHole/One/Uno.py
+++ trials/RabbitHole/One/Uno.py
@@ -1,7 +1,8 @@
 def Rabbit1():
 	try:
-		from Two.Deux import Target
+		from One.Two.Deux import Target
 		print('Target Found')
 		Target()
 	except ImportError:
 		print('Target Lost')
+hello         
